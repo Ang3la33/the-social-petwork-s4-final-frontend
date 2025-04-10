a440c45025b9763c7155e3f3c3437718ac11243d
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-10 02:39:18 -0230

    Still working on styling page

--- social-petwork/src/Pages/Landing.jsx
+++ social-petwork/src/Pages/Landing.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Landing.css';
 import landingImage from '../Assets/Images/landingpicture.png';
 
 function Landing() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    document.body.style.cursor = 'url(../Assets/Images/paw-cursor.png), auto';
+    return () => {
+      document.body.style.cursor = 'auto';
+    }
+  })
 
   return (
     <div className="landing-container">
@@ -14,9 +21,9 @@ function Landing() {
       <div className='text-section'>
         <h1 className='title'>
           The <br />
-          <span>Social</span> <br />
-          <span>Petwork</span> 
-        </h1>
+          Social<br />
+          Petwork
+        </h1>http://localhost:3000/static/media/landingpicture.cb4f2df04f760f21f09a.png
         <p className='subtitle'>A Safe Place For Pet Lovers To Connect</p>
         <div className='button-group'>
           <button onClick={() => navigate('/login')}>Login</button>
