72e4d11650fdb114feac9014c89e611e3f619aa5
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-10 02:27:56 -0230

    Updated Landing Page - need to fix styling

--- social-petwork/src/Pages/Landing.jsx
+++ social-petwork/src/Pages/Landing.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../Landing.css';
+import landingImage from '../Assets/Images/landingpicture.png';
 
 function Landing() {
   const navigate = useNavigate();
 
   return (
-    <div className="landing">
-      <h1>Welcome to the Landing Page</h1>
-      <button onClick={() => navigate('/login')}>Login</button>
-      <button onClick={() => navigate('/register')}>Register</button>
+    <div className="landing-container">
+      <div className='image-section'>
+        <img src={landingImage} alt="Landing Image" className='landing-image' />
+      </div>
+      <div className='text-section'>
+        <h1 className='title'>
+          The <br />
+          <span>Social</span> <br />
+          <span>Petwork</span> 
+        </h1>
+        <p className='subtitle'>A Safe Place For Pet Lovers To Connect</p>
+        <div className='button-group'>
+          <button onClick={() => navigate('/login')}>Login</button>
+          <button onClick={() => navigate('/register')}>Register</button>
+        </div>
+      </div>
     </div>
   );
 }
